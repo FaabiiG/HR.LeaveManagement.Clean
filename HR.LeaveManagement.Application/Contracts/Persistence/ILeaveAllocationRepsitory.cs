b985684b0d9c2ec62a3eefab6e15b473e77fc483
fatbintou gueye <fatbintou16@hotmail.com>
2023-05-30 16:50:06 +0000

    feat: Setting up Infrastructure Projects

--- HR.LeaveManagement.Application/Contracts/Persistence/ILeaveAllocationRepsitory.cs
+++
@@ -1,9 +0,0 @@
-﻿using HR.LeaveManagement.Domain;
-
-namespace HR.LeaveManagement.Application.Contracts.Persistence
-{
-    public interface ILeaveAllocationRepsitory : IGenericRepository<LeaveAllocation>
-    {
-       
-    }
-}
