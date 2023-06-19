2d3d8068c7a08084aa9ef65685b3bef658583e61
fatbintou gueye <fatbintou16@hotmail.com>
2023-06-19 11:34:50 +0000

    feat: Added Unit tests and integration test

--- HR.LeaveManagement.Apo/Program.cs
+++ HR.LeaveManagement.Apo/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
